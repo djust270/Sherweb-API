eb941b180273d472bf62d1f1b7901a3eb79d8c25
David Just <dave.just@gmail.com>
2025-02-27 17:36:56 -0500

    add Get-SherwebCustomerSubscriptionMeters

---
+++ public/Service Provider/Get-SherwebCustomerSubscriptionMeters.ps1
@@ -0,0 +1,51 @@
+<#
+.SYNOPSIS
+    Retrieves the list of subscriptions meters for one of your customers.
+
+.DESCRIPTION
+    The Get-SherwebCustomerSubscriptionMeters function retrieves the list of subscriptions meters for one of your customers.
+
+.PARAMETER CustomerId
+    The ID of the customer to retrieve subscriptions for.
+
+.PARAMETER PlatformId
+    The ID of the platform to retrieve subscriptions for.
+
+.EXAMPLE
+    PS> Get-SherwebCustomerSubscriptionMeters -CustomerId "00000000-0000-0000-0000-000000000000" -PlatformId "00000000-0000-0000-0000-000000000000"
+    Retrieves the list of subscriptions meters for the specified customer/platform.
+
+.OUTPUTS
+    PSCustomObject[]
+    A list of subscriptions meters.
+
+.NOTES
+    Reference: https://developers.sherweb.com/api-details#api=service-provider-api&operation=GetCustomerSubscriptionMeters
+#>
+function Get-SherwebCustomerSubscriptionMeters {
+    
+    [CmdletBinding()]
+    [OutputType([PSCustomObject[]])]
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$CustomerId,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$PlatformId
+    )
+
+    begin {
+        $sherwebParams = @{
+            API         = "ServiceProvider"
+            Endpoint    = "billing/subscriptions/meters"
+            FilterQuery = "customerId=$CustomerId&platformId=$PlatformId"
+            Method      = "GET"
+        }
+    }
+
+    process {
+        Invoke-SherwebRequest @sherwebParams
+    }
+}
