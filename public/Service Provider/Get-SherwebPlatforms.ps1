ea8ab2f84a2d2ce7c6ef29bb47ee9dd1cb017bb5
David Just <dave.just@gmail.com>
2025-03-01 10:29:34 -0500

    Add comment help for Get-SherwebPlatforms

--- public/Service Provider/Get-SherwebPlatforms.ps1
+++ public/Service Provider/Get-SherwebPlatforms.ps1
@@ -1,5 +1,21 @@
 function Get-SherwebPlatforms {
+<#
+.SYNOPSIS
+    Retrieves a list of platforms from Sherweb.
 
+.DESCRIPTION
+    The Get-SherwebPlatforms function retrieves and returns a list of platforms from the Sherweb API.
+
+.EXAMPLE
+    PS> Get-SherwebPlatforms
+    Returns a list of platforms in the Sherweb account.
+
+.OUTPUTS
+    PSCustomObject[]
+    A list of platforms.
+
+.NOTES
+    Reference: https://developers.sherweb.com/api-details#api=service-provider-api&operation=GetAllPlatforms
     [CmdletBinding()]
     [OutputType([PSCustomObject[]])]
     param()
