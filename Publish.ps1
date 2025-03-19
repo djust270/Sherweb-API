78955826ee0f9ae6a5270da90a81e063c385b272
David Just <57944742+djust270@users.noreply.github.com>
2025-03-19 10:10:35 -0400

    Update Publish.ps1

--- Publish.ps1
+++ Publish.ps1
@@ -3,9 +3,9 @@ $TempDir = Join-Path -Path $env:TMPDIR -ChildPath "Sherweb-API"
 New-Item -Path $TempDir -ItemType Directory -Force
 
 # Copy only the files you need
-Copy-Item -Path "$PSScriptRoot\docs" -Destination $TempDir
-Copy-Item -Path "$PSScriptRoot\private" -Destination $TempDir
-Copy-Item -Path "$PSScriptRoot\public" -Destination $TempDir
+Copy-Item -Path "$PSScriptRoot\docs" -Destination $TempDir -Recurse
+Copy-Item -Path "$PSScriptRoot\private" -Destination $TempDir -Recurse
+Copy-Item -Path "$PSScriptRoot\public" -Destination $TempDir -Recurse
 Copy-Item -Path "$PSScriptRoot\*.psd1" -Destination $TempDir
 Copy-Item -Path "$PSScriptRoot\*.psm1" -Destination $TempDir
 Copy-Item -Path "$PSScriptRoot\LICENSE" -Destination $TempDir
@@ -13,6 +13,3 @@ Copy-Item -Path "$PSScriptRoot\LICENSE" -Destination $TempDir
 
 # Publish the module from the temp directory
 Publish-Module -Path $TempDir -NuGetApiKey $env:APIKEY
-
-# Cleanup (optional - GitHub Actions cleans up after itself)
-Remove-Item -Path $TempDir -Recurse -Force
